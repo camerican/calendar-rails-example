e97db81f47beefa2e0d82d4e2c320245c9b5fcbb
Cam Crews <camcrews@gmail.com>
2017-02-27 12:06:02 -0500

    Delete helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,6 +3,12 @@ module EventsHelper
     content_tag :li, "#{event.name} (#{days_out event})"
   end
 
+  def delete_link event
+    link_to event, method: :delete do
+      image_tag 'http://www.clipartbest.com/cliparts/pc5/XKE/pc5XKEqxi.jpg', width: 50
+    end
+  end
+
   def days_out event
     distance_of_time_in_words(Time.now, event.start) + " from now"
   end
