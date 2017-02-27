b27da020850892b33c3690885e17a0906c8c18fe
Cam Crews <camcrews@gmail.com>
2017-02-27 11:41:50 -0500

    View helper examples and partial examples

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def output event
+    content_tag :li, "#{event.name} (#{days_out event})"
+  end
+
+  def days_out event
+    distance_of_time_in_words(Time.now, event.start) + " from now"
+  end
+
+  def duration
+    distance_of_time_in_words event.start, event.end
+  end
 end
